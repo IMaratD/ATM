bdb257b9d28788d38854664459227100f47367cb
Marat <imd_90@mail.ru>
2020-11-07 13:45:26 +0300

    Fixed #1

--- src/ATM.java
+++ src/ATM.java
@@ -1,34 +1,20 @@
 public class ATM {
     private long moneyAmount;
-    private long sum;
 
-//    private int banknote;
-//    private long banknotesNum;
-
-
-    public ATM(long moneyAmount, long sum) {
-        this.moneyAmount = moneyAmount;
-        this.sum = sum;
-    }
-
-    public long getMoneyAmount() {
-        return moneyAmount;
-    }
-
-    public void setMoneyAmount(long moneyAmount) {
+    public ATM(long moneyAmount) {
         this.moneyAmount = moneyAmount;
     }
 
-    public long getSum() {
-        if (sum > moneyAmount) {
+    public long takeMoney(long takeMoney) {
+        if (takeMoney > moneyAmount) {
             return 0;
         }
-        return sum;
+        takeMoney = moneyAmount - takeMoney;
+        return takeMoney;
     }
 
-    public long setSum(long sum) {
-        this.moneyAmount = sum + moneyAmount;
-
+    public long putMoney(long putMoney) {
+        moneyAmount = moneyAmount + putMoney;
         return moneyAmount;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        ATM atm = new ATM(
-                1_000_000_00,
-                10_000_000_00);
-        System.out.println(atm.getSum());
-
-        System.out.println(atm.setSum(500_00));
+        ATM atm = new ATM(1_000_000_00);
+        atm.putMoney(1000_00);
+        atm.takeMoney(10_000_00);
     }
 }
